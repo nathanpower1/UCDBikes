00cc898dd80d0045501bcba3ba55be23f7b6f7b7
Niallmckayy <niallmckay71@gmail.com>
2024-02-21 11:15:21 +0000

    Updated

--- AvailabilityScraper.py
+++ AvailabilityScraper.py
@@ -3,30 +3,38 @@ import json
 import mysql.connector
 from datetime import datetime
 import logging
-from dataScraper import DatabaseManager, StationDataHandler
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)  # Set logging level to INFO
-
 
 HOST = "dublinbikes.c1ywqa2sojjb.eu-west-1.rds.amazonaws.com"
 USER = "admin"
 PASSWORD = "boldlynavigatingnature"
 DATABASE = "dublinbikes"
+
+database = DatabaseManager(HOST, USER, PASSWORD, DATABASE)
+
+connection = mysql.connector.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE
+        )
+cursor = connection.cursor()
+
 contract = 'dublin'
 api_key = '954118b06527f2a603d5abd3c315876b16221c14'
 stations_url = f"https://api.jcdecaux.com/vls/v1/stations?contract={contract}&apiKey={api_key}"
 
-# Establish a connection to the database
-database = DatabaseManager(HOST, USER, PASSWORD, DATABASE)
+try:
+    response = requests.get(stations_url)
+    if response.status_code == 200:
+        stationData =  json.loads(response.text)
+    else:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
 
-# Fetching the cursor object
-station_data_handler = StationDataHandler(contract, api_key)
-
-# Fetching station information from the API
-stationData = station_data_handler.get_station_info()
-
-# Inserting data into the database
 if stationData:
     for data in stationData:
         number = data.get('number')
@@ -40,8 +48,8 @@ if stationData:
         availabilityData = (number, last_update, available_bikes, available_bike_stands, status, unix_timestamp)
         
         select_query = "SELECT * FROM availability WHERE number = %s AND last_update = %s"
-        database.cursor.execute(select_query, (number, last_update))
-        existing_entry = database.cursor.fetchone()
+        cursor.execute(select_query, (number, last_update))
+        existing_entry = cursor.fetchone()
         if existing_entry:
             logging.info(f"Entry with number {number} and last_update {last_update} already exists. Skipping insertion.")
             continue  # Skip insertion and move to the next iteration
@@ -49,11 +57,11 @@ if stationData:
             logging.info(f"No existing entry found for number {number} and last_update {last_update}. Proceeding with insertion.")
         insert_query = "INSERT INTO availability (number, last_update, available_bikes, available_bike_stands, status, timestamp) VALUES (%s, %s, %s, %s, %s, %s)"
         try:
-            database.cursor.execute(insert_query, availabilityData)
-            database.connection.commit()
+            cursor.execute(insert_query, availabilityData)
+            connection.commit()
             logging.info("Insert executed successfully.")
         except mysql.connector.Error as e:
-            database.connection.rollback()
+            connection.rollback()
             logging.error(f"Error executing insert: {e}")
             print(f"Error executing insert: {e}")
 
