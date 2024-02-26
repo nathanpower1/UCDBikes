51cf4fe1f8fff368c2a7135079a99a9f28731b25
Niallmckayy <niallmckay71@gmail.com>
2024-02-26 11:34:21 +0000

    Updated

--- AvailabilityScraper.py
+++ AvailabilityScraper.py
@@ -67,56 +67,24 @@ if stationData:
             print(f"Error executing insert: {e}")
 
 
-if count % 12 == 0:
-    #checks if there is any changes to the static station data every hour, 
-    #this checks the station numbers in our SQL table, against the numbers pulled from the API. 
-    #If there is a number missing from the MySQL that is seen in the API, then we add a row. 
-    #If there is a number in MySQL but missing from the API information, then we remove that row.
-    print("Checking for changes in static station data.")
-    cursor.execute("SELECT number FROM station")
-    station_numbers = cursor.fetchall()
-    station_numbers = [x[0] for x in station_numbers]
-    api_station_numbers = [x['number'] for x in stationData]
-    missing_numbers = list(set(api_station_numbers) - set(station_numbers))
-    extra_numbers = list(set(station_numbers) - set(api_station_numbers))
-    for number in missing_numbers:
-        for data in stationData:
-            if data['number'] == number:
-                address = data.get('address')
-                banking = int(data.get('banking'))
-                bike_stands = data.get('bike_stands')
-                bonus = int(data.get('bonus'))
-                contract = data.get('contract_name')
-                name = data.get('name')
-                number = data.get('number')
-                lat = data.get('position').get('lat')
-                lng = data.get('position').get('lng')
-                status = data.get('status')
-                values = (address, banking, bike_stands, bonus, contract, name, number, lat, lng, status)
-                insert_query = "INSERT INTO station (address, banking, bike_stands, bonus, contract_name, name, number, position_lat, position_lng, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                try:
-                    cursor.execute(insert_query, values)
-                    connection.commit()
-                    logging.info(f"Station {name} inserted successfully.")
-                except mysql.connector.Error as e:
-                    connection.rollback()
-                    logging.error(f"Error executing insert: {e}")
-                    print(f"Error executing insert: {e}")
-    for number in extra_numbers:
-        delete_query = "DELETE FROM station WHERE number = %s"
-        try:
-            cursor.execute(delete_query, (number,))
-            connection.commit()
-            logging.info(f"Station {number} deleted successfully.")
-        except mysql.connector.Error as e:
-            connection.rollback()
-            logging.error(f"Error executing delete: {e}")
-            print(f"Error executing delete: {e}")
+# Fetch weather data
+ISO = 'IRL'
 
-count += 1
-print(f"Count: {count}")
+city = 'dublin'
+postcode = 'D1'
 
+weather_api_key = '53cca80e47157e1ee9b5778f95c90c41'
+weather_api = f"https://api.openweathermap.org/data/2.5/weather?lat=53.3498&lon=6.2603&appid={weather_api_key}"
 
+try:
+    response = requests.get(weather_api)
+    if response.status_code == 200:
+        weatherData =  json.loads(response.text)
+        rain = weatherData.get('rain')
+    else:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
 
-
+# Add weather data to database
 
