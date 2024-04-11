cf4a47d8aa7bf7c4eb3e10f733f1beff5065316e
Niallmckayy <niallmckay71@gmail.com>
2024-04-11 09:27:40 +0100

    Next batch of historical weather

--- historicalWeatherData.py
+++ historicalWeatherData.py
@@ -38,10 +38,11 @@ APIKey4 = 'b3e4f3860e1b82f0aa34c664bab9709a'
 #0-700 done with APIKey
 #-700: done with APIKey2
 #-1400:-701 with APIKey
+#-2100:-1400 with APIKey2
 
-for timestamp in missing_timestamps_unique[-1400:-701]:
+for timestamp in missing_timestamps_unique[-2100:-1400]:
 
-    weather_api = f'https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={timestamp}&appid={APIKey}'
+    weather_api = f'https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={timestamp}&appid={APIKey2}'
     try:
         response = requests.get(weather_api)
         if response.status_code == 200:
